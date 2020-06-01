6f8b8200a1f6d1deb457ded77f0fd87db53a825b
someone <someone@someplace.com>
2020-06-01 14:20:59 +0200

    unfinished stuff

--- js/sites_edit.js
+++ js/sites_edit.js
@@ -1,27 +1,84 @@
 "use strict"
 
-function addHeaderEvent(e) {
-    var deleteElem = document.getElementById("article1"); 
-    deleteElem.textContent = '';
-    
+function addId() {
+    var allEditArticles = document.querySelectorAll(".edit_article");
+    var countId = 0;
+    allEditArticles.forEach( (elem) => {
+        elem.setAttribute("id", "editArticle" + countId);
+        countId++;
+    });
+}
+
+function drawArticle(header) {
+    var article = document.createElement("article"); 
+    article.setAttribute("class", "edit_article");
+    var wrapper = document.getElementById("sites_wrapper");
+    wrapper.appendChild(article);
+
     var editButton = document.createElement("button");
     editButton.setAttribute("class", "editButton");
     editButton.innerHTML = "edit article"; 
-    deleteElem.appendChild(editButton);
-    
+    article.appendChild(editButton);
+
     var textArea = document.createElement("textarea");
     textArea.setAttribute("class", "headerTextarea");
-    deleteElem.appendChild(textArea); 
+    textArea.setAttribute("id", "headerTextareaSave");
+    textArea.innerHTML = header;
+    article.appendChild(textArea); 
     
     var saveButton = document.createElement("button");
     saveButton.setAttribute("class", "saveButton");
     saveButton.innerHTML = "save"; 
-    deleteElem.appendChild(saveButton);
-    
+    article.appendChild(saveButton);
+
+    var deleteButton = document.createElement("button");
+    deleteButton.setAttribute("class", "deleteButton");
+    deleteButton.innerHTML = "delete"; 
+    article.appendChild(deleteButton);
+    deleteButton.addEventListener("click", deleteArticle);
     saveButton.addEventListener("click", saveArticle); 
 }
 
-function saveArticle() {
+function deleteArticle(e) {
+    var parentNode = e.currentTarget.parentNode; 
+    var parentId = parentNode.id;
+    
+    var counter = 11;
+    var isNumber = true;
+    var numberId = "";
+    while (isNumber) {
+        if (parentId.charAt(counter)) {
+            isNumber = true; 
+            numberId += parentId.charAt(counter); 
+        } else {
+            isNumber = false;
+        }
+        counter++;
+    }
+
+    var xmlhttp0 = new XMLHttpRequest();
+    xmlhttp0.addEventListener('readystatechange', (e) => {
+        if (xmlhttp0.readyState==4 && xmlhttp0.status==200) {
+            var responseText = xmlhttp0.responseText;
+            console.log(responseText);
+        }
+    });
+    xmlhttp0.open('GET', "php/save_article.php?delete=true&index=" + numberId, true);
+    xmlhttp0.send();    
+
+    parentNode.remove();
+    addId();
+}
+
+function addHeaderEvent(e) {
+    var header = document.getElementById("headerTextarea").value;
+    drawArticle(header);
+    addId();
+}
+
+function saveArticle(e) {
+    var parentId = e.currentTarget.parentNode.id; 
+    var header = document.querySelector("#" + parentId + " textarea").value; 
     var xmlhttp0 = new XMLHttpRequest();
     xmlhttp0.addEventListener('readystatechange', (e) => {
         if (xmlhttp0.readyState==4 && xmlhttp0.status==200) {
@@ -31,8 +88,7 @@ function saveArticle() {
     });
     xmlhttp0.open('POST', "php/save_article.php", true);
     xmlhttp0.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp0.send();    
-}
+    xmlhttp0.send("header=" + header);    
 }
 
 function addAddButton(article) {
@@ -47,6 +103,7 @@ function addAddButton(article) {
 function addHeaderTextarea(article) {
     var textArea = document.createElement("textarea");
     textArea.setAttribute("class", "headerTextarea");
+    textArea.setAttribute("id", "headerTextarea");
     article.appendChild(textArea); 
 }
 
@@ -62,6 +119,20 @@ function addOneEditBlock() {
     var editArticle = makeEditArticle();
     addAddButton(editArticle); 
     addHeaderTextarea(editArticle);
+
+    var xmlhttp0 = new XMLHttpRequest();
+    xmlhttp0.addEventListener('readystatechange', (e) => {
+        if (xmlhttp0.readyState==4 && xmlhttp0.status==200) {
+            var json = JSON.parse(xmlhttp0.responseText);
+            console.log(json);
+            json.forEach( (elem) => {
+                drawArticle(elem);
+            }); 
+            addId();
+        }
+    });
+    xmlhttp0.open("GET", "php/save_article.php?getArticle=true");
+    xmlhttp0.send(); 
 }
 
 document.addEventListener('DOMContentLoaded', addOneEditBlock);
--- php/save_article.php
+++ php/save_article.php
@@ -1,2 +1,134 @@
 <?php
 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "save_article";
+
+function conn() {
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "save_article";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    return $conn;
+}
+$conn = new mysqli($servername, $username, $password);
+
+$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+if ($conn->query($sql) === TRUE) {
+  //echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+$conn->close();
+
+$sql = "CREATE TABLE IF NOT EXISTS save_articles (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    header VARCHAR(100), 
+    realId INT 
+)";
+
+$conn = conn();
+if ($conn->query($sql) === true) {
+    //echo "Table pages_data created successfully";
+} else {
+   echo "Error creating table: " . $conn->error;
+}
+$conn->close();
+
+
+function addArticleDB($header) {
+    $sql = "INSERT INTO save_articles (header)
+                 VALUES (?)";
+    $conn = conn();
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $header);
+    $stmt->execute();
+ 
+}
+
+function getArticleDB() {
+    $sql = "select * from save_articles";    
+    $conn = conn();
+    $result = $conn->query($sql);
+    $array = array();
+    if ($result->num_rows > 0) {
+        $count = 0;
+        while ($row = $result->fetch_assoc()) {
+            $array[$count] = array($row["header"]);
+            $count++;
+        }
+
+    }
+    return $array;
+}
+
+function setArticleId($index = -1) {
+    $conn = conn();
+    $sql = "SELECT id FROM save_articles LIMIT 1";
+    $result = $conn->query($sql);
+    $startId = -1;
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $startId = $row["id"]; 
+        }
+    } else {
+        echo "0 results";
+    }
+     
+    $conn = conn();
+    $sql = "SELECT count(*) as total from save_articles";
+    $result = $conn->query($sql);
+    $data = $result->fetch_assoc();
+    
+    $conn = conn();
+    $once = false;
+    for ($i = 0; $i < $data["total"]; ++$i) {
+        if ($index != -1) {
+            if ($i == $index && !$once) {
+                $startId++;
+                $once = true;
+            }
+        }
+ 
+        $sql = "UPDATE save_articles SET realId=$i WHERE id=$startId"; 
+        if ($conn->query($sql) == true) {
+            //echo "Updating sucessfull";
+        } else {
+            echo "Error updating record: " . $conn->error;
+        }
+        $startId++;
+    }
+}
+
+function deleteArticle($index) {
+    setArticleId();     
+    $conn = conn(); 
+    $sql = "DELETE FROM save_articles WHERE realId=$index";
+    
+    if ($conn->query($sql) === true) {
+        //echo "Deleted successful data"; 
+    } else {
+        echo "Error deleting data: " . $conn->error();
+    }
+    setArticleId($index);
+}
+
+if (isset($_GET["delete"], $_GET["index"])) {
+    deleteArticle($_GET["index"]);
+}
+
+if (isset($_POST["header"])) {
+    echo $_POST["header"];
+    addArticleDB($_POST["header"]); 
+}
+
+if (isset($_GET["getArticle"])) {
+    echo json_encode(getArticleDB()); 
+}
